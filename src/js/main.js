808a5190c67d361faed0151b8c00a3c1e7ab2dc5
Tigran <tigrankvardanyan2006@gmail.com>
2023-06-19 10:54:43 -0700

    branding1

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import "./libs/dinamic-adaptiv.js";
+import "./libs/slider.js";
+import "./module/burger-menu.js";
+import "./module/accordion.js"
+import "./module/customSelect.js"
+import "./module/lang.js"
+import "./module/scrollGoTo.js"
+import "./polyfills/focus-visible.js"
+import "./polyfills/inert.js"
+
